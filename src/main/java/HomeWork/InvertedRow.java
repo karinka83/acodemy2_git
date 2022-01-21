9515c37839e68faec268cc71d3fe4a8c689c32e7
karinka83 <karina.kulakova@gmail.com>
2022-01-21 23:25:40 +0200

    [feature/java2] add a file "InvertedRow" to a second HomeWork

---
+++ src/main/java/HomeWork/InvertedRow.java
@@ -0,0 +1,13 @@
+package HomeWork;
+
+public class InvertedRow {
+    public static void main(String[] args) {
+        //Task Nr. 07
+
+        int numberWithZero = 50;
+        for (int i = numberWithZero; i >= 1; i--) {
+            //if ((i + 1) == numberWithTwoZero-1);
+            System.out.print((i) + ",");
+        }
+    }
+}
